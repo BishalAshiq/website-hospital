8a509af38dc025997e10d7e5bd3133b315f68a54
BishalAshiq <bishaltheboss20@gmail.com>
2022-05-23 01:34:24 +0600

    updated extraTemp

--- src/Pages/Home/ExtraTemp/ExtraTemp.js
+++ src/Pages/Home/ExtraTemp/ExtraTemp.js
@@ -9,26 +9,22 @@ const ExtraTemp = () => {
       style={{
         backgroundImage: `url("https://i.ibb.co/GdrDJYR/extra-Temp.png")`,
         backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        paddingBottom: "10px",
+        backgroundSize: "cover"
       }}
     >
       <Container>
         <div className="section">
-          <div className="temp-img">
-            <img src={doctors} alt="" />
-          </div>
           <div>
             <h1 className="temp-h1">
               Make Your Appointment <br /> Today
             </h1>
             <p className="temp-p">
               People with health problems who visit the hospital for diagnosis
-              or <b />
-              treatment, but do not at this time require a bed or to be admitted{" "}
-              <b />
-              for overnight care.
+              or  treatment, but do not at this time require a bed or  to be admitted for overnight care.
             </p>
+          </div>
+          <div className="temp-img">
+            <img src={doctors} alt="" />
           </div>
         </div>
       </Container>
