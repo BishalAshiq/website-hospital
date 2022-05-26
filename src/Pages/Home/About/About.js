42f39ba06889095bcf9c240633f9c7b5318625c5
BishalAshiq <bishaltheboss20@gmail.com>
2022-05-26 23:09:46 +0600

    updated aboutSection

--- src/Pages/Home/About/About.js
+++ src/Pages/Home/About/About.js
@@ -11,12 +11,20 @@ const About = () => {
   return (
     <div>
       <Container>
-        <section id="about">
-          <div className="img">
-            <img src={about4} alt="" />
+        <section id="about" data-aos="fade-up"
+     data-aos-anchor-placement="center-center">
+          <div className="img" data-aos="fade-right">
+            <img
+              style={{ paddingTop: 40, width: 400, height: 250 }}
+              src={about4}
+              alt=""
+            />
           </div>
-          <div className="article">
-            <article>
+          <div className="article" data-aos="fade-right">
+            <h1>
+              <u>Radiation Hospital</u>
+            </h1>
+            <article style={{ textAlign: "justify" }}>
               Scholars have speculated that the metaverse will alter the way in
               which the hospitality and tourism industry operates. Efforts to
               understand this new phenomenon in both academia and industry are
@@ -27,15 +35,16 @@ const About = () => {
               identifying research gaps, and proposing agenda items with the
               potential to significantly benefit hospitality and tourism
               industry players. Finally, we classify future research agendas
-              into three broad categories: staging experiences in the metaverse,
-              understanding possible changes in the consumer behavior, and
-              marketing and operations strategies in the metaverse.
+              into three broad categories.
             </article>
           </div>
         </section>
         <section id="about">
-          <div className="article">
-            <article>
+          <div className="article" data-aos="fade-right">
+            <h1>
+              <u>Covid-19 Cabin</u>
+            </h1>
+            <article style={{ textAlign: "justify", marginLeft: 10 }}>
               In areas where the virus that causes COVID-19 is circulating,
               staff aged 60 or over, or who have any underlying health
               conditions such as heart disease, diabetes or lung cancer, should
@@ -48,16 +57,27 @@ const About = () => {
               policies and regulations.
             </article>
           </div>
-          <div className="img">
-            <img src={about2} alt="" />
+          <div className="img" data-aos="fade-left">
+            <img
+              style={{ paddingTop: 40, width: 400, height: 250 }}
+              src={about2}
+              alt=""
+            />
           </div>
         </section>
         <section id="about">
-          <div className="img">
-            <img src={about1} alt="" />
+          <div className="img" data-aos="fade-right">
+            <img
+              style={{ paddingTop: 40, width: 400, height: 250 }}
+              src={about1}
+              alt=""
+            />
           </div>
-          <div className="article">
-            <article>
+          <div className="article" data-aos="fade-left">
+            <h1>
+              <u>Staff Hospitality</u>
+            </h1>
+            <article style={{ textAlign: "justify" }}>
               As fever, dry cough or tiredness, staff at the accommodation
               establishment should contact the local health authority and follow
               their instructions. The sick person should be isolated in a room,
@@ -68,41 +88,50 @@ const About = () => {
               room, if possible. It is usually the relevant public health
               authority, not the management of the hotel and accommodation
               establishment, which has the authority to demand sick guests to
-              temporarily remain in their room or to prevent them from receiving
-              visits from other guests. National law will guide the rights of
-              the guests to refuse or not the recommended measures. Staff
-              entering the room of an ill person should maintain at least a 1
-              metre distance from the ill person, and request that the ill
-              person put on a medical mask.
+              temporarily remain in their room.
             </article>
           </div>
         </section>
         <section id="about">
-          <div className="article">
-            <article>
-              The aim of this paper is to estimate households’ willingness to
-              pay (WTP) for the development of community based tourism (CBT)
-              around Alelo Bad hot spring water. To achieve the stated
-              objective, relevant data was collected from a total of 157 sample
-              respondents through questionnaires, interviews and field
-              observation techniques. In this study, both descriptive and
-              econometric models (Seemingly Unrelated Bivariate Probit and
-              Probit models) were used to analyze the collected data. The
-              descriptive statistics clearly shows that 66.24% of the
-              respondents were willing to pay for the existence of the proposed
-              project while 33.76% were not willing to pay.
+          <div className="article" data-aos="fade-right">
+            <h1>
+              <u>Cabin Items</u>
+            </h1>
+            <article style={{ textAlign: "justify", marginLeft: 10 }}>
+              The aim of this paper is to estimate households willingness to pay
+              (WTP) for the development of community based tourism (CBT) around
+              Alelo Bad hot spring water. To achieve the stated objective,
+              relevant data was collected from a total of 157 sample respondents
+              through questionnaires, interviews and field observation
+              techniques. In this study, both descriptive and econometric models
+              (Seemingly Unrelated Bivariate Probit and Probit models) were used
+              to analyze the collected data. The descriptive statistics clearly
+              shows that 66.24% of the respondents were willing to pay for the
+              existence of the proposed project while 33.76% were not willing to
+              pay.
             </article>
           </div>
-          <div className="img">
-            <img src={about3} alt="" />
+          <div className="img" data-aos="fade-left">
+            <img
+              style={{ paddingTop: 40, width: 400, height: 250 }}
+              src={about3}
+              alt=""
+            />
           </div>
         </section>
-        <section id="about">
-          <div className="img">
-            <img src={about5} alt="" />
+        <section id="about" style={{marginBottom: 80}}>
+          <div className="img" data-aos="fade-right">
+            <img
+              style={{ paddingTop: 40, width: 400, height: 250 }}
+              src={about5}
+              alt=""
+            />
           </div>
-          <div className="article">
-            <article>
+          <div className="article" data-aos="fade-left">
+            <h1>
+              <u>Operation Theater</u>
+            </h1>
+            <article style={{ textAlign: "justify", marginLeft: 10 }}>
               Being green has become a focus in hospitality as evidenced by the
               rise of sustainable tourism in general and green hotels in
               particular. The current research examines the impact of
@@ -113,10 +142,7 @@ const About = () => {
               strategy (i.e., underdog versus top dog), which signals
               quality-based information (Study 2), and its green advertising
               appeals (i.e., competence versus warmth), which signal both
-              quality-based and intent-based information (Study 3). Our research
-              offers a theoretical framework for hotels to develop proper market
-              signals to promote their green focus and it provides guidance for
-              effective brand positioning and advertising strategies.
+              quality-based and intent-based information.
             </article>
           </div>
         </section>
--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -1,9 +1,36 @@
 import React from 'react';
-
+import './Footer.css';
 const Footer = () => {
     return (
-        <div>
-            <h1>This Is Footer</h1>
+        <div className='footer' data-aos="zoom-in-up">
+            <div>
+                <p>Emergency Care Of Patients</p>
+                <p>Check Up</p>
+                <p>Treatment of Personal Diseases</p>
+                <p>Treatment Extractions</p>
+                <p>Check Up</p>
+            </div>
+            <div>
+                <h2 className='f-h1'>Services</h2>
+                <p>Fluoride Treatment</p>
+                <p>Corona Virus Treatment</p>
+                <p>Cardiology</p>
+                <p>Orthopedics Specialist</p>
+                <p>Kidney Specialist</p>
+            </div>
+            <div>
+                <h2 className='f-h1'>Policies</h2>
+                <p>Personal Diseases</p>
+                <p>Settings</p>
+                <p>Cookies</p>
+                <p>Treatment Extractions</p>
+                <p>Check Up</p>
+            </div>
+            <div>
+                <h2 className='f-h1'>Our Address</h2>
+                <p>Dhaka Bangladesh - 1216 Dhanmondi-15</p>
+                
+            </div>
         </div>
     );
 };
